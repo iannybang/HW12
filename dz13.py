8f8630ede73a75c6e964b5ddeee7ec8fae992f7a
iannybang <iannybang@gmail.com>
2023-11-01 22:47:20 -0400

    created new branch and updated with new dz13 files

---
+++ dz13.py
@@ -0,0 +1,41 @@
+import json
+import os
+import csv
+
+from genres import ganres
+
+#Крок1
+genre_list = json.loads(ganres)
+
+#Крок2
+os.mkdir('Genres')
+os.chdir('Genres')
+
+for genre in genre_list['results']:
+    os.mkdir(genre['genre'])
+
+
+#Крок3
+csv_columns =['title','year','rating','type','genres']
+csv_file_name = "film_info.csv"
+
+for folder in os.listdir():
+  csv_file_path = os.path.join(folder, csv_file_name)
+  with open(csv_file_path, 'w',newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(csv_columns)
+
+#Крок4
+
+from films_data import films_data
+
+for folder in os.listdir():
+    csv_file_name = os.path.join(folder, "film_info.csv")
+    for film in films_data:
+        for genre in film['gen']:
+            if genre['genre'] == folder:
+                csv_file_path =  csv_file_name
+                with open(csv_file_path,'a') as csv_file:
+                    csv_writer = csv.writer(csv_file)
+                    csv_writer.writerow([film['title'],film['year'],film['rating'],film['type'], ', '.join(genre['genre'] for genre in film['gen'])])
+
