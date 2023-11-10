f65a652ae026be666a9355bddb7bda901b68f437
iannybang <iannybang@gmail.com>
2023-11-10 11:43:18 -0500

    modified files

--- film_player/film_storage.py
+++ film_player/film_storage.py
@@ -1,12 +1,10 @@
 #Create directory to store files with film information
 import os
+import string
 
 new_dir = 'film_player/film_storage'
 os.makedirs (new_dir )
 os.chdir (new_dir)
 
-import string
-
 for letter in string.ascii_uppercase:
       os.mkdir(letter)
-
